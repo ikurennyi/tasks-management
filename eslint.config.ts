4614f6a2a1757aac5bd424b9b627c7ad27cfbe3d
Ievgenii Kurennyi <ievgeniikurennyi@cloud.upwork.com>
2025-03-27 15:29:04 +0100

    feat: ESLint and Prettier updates
    - addded pre-commit hook with Husky and lint-staged
    - Prettier: trailingComma, brackedSpacing, bracketSameLine
    - ESLint: sort Vue attributes according to the recommended rules

--- eslint.config.ts
+++ eslint.config.ts
@@ -21,15 +21,15 @@ export default defineConfigWithVueTs(
     ignores: ['**/dist/**', '**/dist-ssr/**', '**/coverage/**'],
   },
 
-  pluginVue.configs['flat/essential'],
+  pluginVue.configs['flat/recommended'],
   vueTsConfigs.recommended,
-  
+
   {
     ...pluginCypress.configs.recommended,
     files: [
       '**/__tests__/*.{cy,spec}.{js,ts,jsx,tsx}',
       'cypress/e2e/**/*.{cy,spec}.{js,ts,jsx,tsx}',
-      'cypress/support/**/*.{js,ts,jsx,tsx}'
+      'cypress/support/**/*.{js,ts,jsx,tsx}',
     ],
   },
   skipFormatting,
