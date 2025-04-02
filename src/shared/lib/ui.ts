68e3aff038cfc6999f50298268534708bd8008bc
Ievgenii Kurennyi <ievgeniikurennyi@cloud.upwork.com>
2025-04-02 15:58:52 +0200

    feat: GitHub pages. Deploy #2

--- src/shared/lib/ui.ts
+++
@@ -1,7 +0,0 @@
-export function capitalizeFirstLetter(string: string): string {
-  return String(string).charAt(0).toUpperCase() + String(string).slice(1)
-}
-
-export function formatDate(date: Date) {
-  return new Date(date).toISOString().split('T')[0].replace(/-/g, '-')
-}
--- src/shared/ui/AppLoading.vue
+++
@@ -1,6 +0,0 @@
-<template>
-  <div class="d-flex align-center flex-column">
-    <h2 class="mb-6">Loading...</h2>
-    <v-progress-linear :height="10" rounded color="primary" indeterminate></v-progress-linear>
-  </div>
-</template>
